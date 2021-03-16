b7fe869c8955df1ebeb1575ecf6565a57f3406f8
Mario Lucca <mario-lucca@hotmail.com>
2021-03-16 13:25:47 -0300

    Initial commit

---
+++ src/components/Titulo/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './estilo.css';
+
+function Titulo(){
+    return(
+      <div className="box">
+          <p className="texto">
+            Baralho dos signos
+          </p>
+      </div>  
+    );
+}
+
+export default Titulo;
